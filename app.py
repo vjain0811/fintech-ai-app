4fc2f4531104ddcb5c41b2f83f50869b1e1ece43
vjain0811 <vjain.0811@gmail.com>
2026-02-22 23:46:35 +0530

    fixed render port

--- app.py
+++ app.py
@@ -149,4 +149,7 @@ def portfolio():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
